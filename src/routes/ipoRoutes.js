2bbdfb9f4ee1eaf9a511c08ad8cef38b8eefab60
Ujjwal Chaturvedi <56334250+Ujjwalch@users.noreply.github.com>
2026-01-15 02:22:38 +0530

    fix scrapper

--- src/routes/ipoRoutes.js
+++ src/routes/ipoRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const ipoController = require('../controllers/ipoController');
 
+// Refresh IPO data (scrape and store) - MUST be before /:id route
+router.post('/refresh', ipoController.refreshIPOs);
+
 // Get all IPOs
 router.get('/', ipoController.getAllIPOs);
 
-// Get IPO details by ID
+// Get IPO details by ID - MUST be last
 router.get('/:id', ipoController.getIPODetails);
-
-// Refresh IPO data (scrape and store)
-router.post('/refresh', ipoController.refreshIPOs);
 
 module.exports = router;
