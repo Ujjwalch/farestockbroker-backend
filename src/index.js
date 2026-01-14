fabdcda37fa99a2159d9a36977f07c491d7a5830
Ujjwal Chaturvedi <56334250+Ujjwalch@users.noreply.github.com>
2026-01-14 23:36:44 +0530

    limit update

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use('/api/brokers', brokerRoutes);
 app.use('/api/admin', adminRoutes);
